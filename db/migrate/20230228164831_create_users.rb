e5a1a9cfdcc66596c2b9dbb547306944a7be28e8
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2023-03-01 12:12:52 +0200

    Update the migration to create the relationship

--- db/migrate/20230228164831_create_users.rb
+++ db/migrate/20230228164831_create_users.rb
@@ -1,12 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :name, null: false
+      t.string :name
       t.text :bio
       t.string :photo
-      t.timestamps :updated_at
-      t.timestamps :created_at
-      t.integer :posts_counter
 
       t.timestamps
     end
--- db/migrate/20230228171352_create_posts.rb
+++ db/migrate/20230228171352_create_posts.rb
@@ -1,11 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.string :title, null :false
+      t.string :title
       t.text :text
-      t.references :author_id, null: false, foreign_key :true
-      t.timestamp :updated_at
-      t.timestamp :created_at
 
       t.timestamps
     end
