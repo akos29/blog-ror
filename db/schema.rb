3b78a9dd84cbc215c6036fd83c44f023af4bfcf6
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2023-03-01 19:51:51 +0200

    Update schema adding comments_counter to Posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_01_161010) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_01_174924) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_161010) do
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
     t.integer "likes_counter"
+    t.integer "comments_counter"
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
