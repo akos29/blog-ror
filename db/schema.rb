32ed3ef3e7bddb26291e658ca6cb3315e147aaae
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2023-03-01 18:13:12 +0200

    Update posts and likes counter to Users and Posts model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_01_105548) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_01_161010) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_105548) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
+    t.integer "likes_counter"
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
@@ -53,6 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_105548) do
     t.string "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "posts_counter"
   end
 
   add_foreign_key "comments", "posts"
