407f4c25fc4c78173c701a0de4712726eb32fbbf
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2023-03-02 17:50:30 +0200

    Fix Linters

--- app/models/like.rb
+++ app/models/like.rb
@@ -10,9 +10,9 @@
 #
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  belongs_to :post, counter_cache:true
+  belongs_to :post, counter_cache: true
 
-  # updates likes counter 
+  # updates likes counter
   def update_likes_counter
     post.increment!(:likes_counter)
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -20,6 +20,7 @@ class Post < ApplicationRecord
   def update_posts_counter
     author.increment!(:posts_counter)
   end
+
   # returns the 5 most recent comments for a given post.
   def show_recent_comments
     comments.where(post_id: id).first(5)
