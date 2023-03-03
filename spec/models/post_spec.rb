fec927e0d1aed99b10b0ef84ebaddcfacb9bc00c
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2023-03-03 20:48:42 +0200

    Add unit test for Post

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  user = User.create(name: 'Kibre', photo: 'link to photo', bio: 'Rails Developer')
+
+  subject {  Post.new(author: user, title: 'About Rais Rspec', text: 'Hello') }
+
+  before { subject.save }
+
+  it 'Title must not be blank' do
+    subject.title = nil
+    expect(subject).to_not be_valid 
+  end
+  
+  it 'Title must not exceed 250 characters' do
+    subject.title = 'a'*250
+    expect(subject).to_not be_valid 
+  end
+  
+  it 'Comments counter must be greater than or equal to 0' do
+    subject.comments_count = -1
+    expect(subject).to_not be_valid 
+  end
+ 
+  it 'Likes counter must be greater than or equal to 0' do
+    subject.likes_count = -1
+    expect(subject).to_not be_valid 
+  end
+end
