1d7bc6c677dbe3bba1972a7a751d98434d7c38f7
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2023-03-03 21:34:58 +0200

    add some unit tests for the User and Post model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Like, type: :model do
   user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
   myPost = Post.create(author: user, title: 'Hello', text: 'This is my first post')
 
-  subject { Like.new(post:myPost, author: user) }
+  subject { Like.new(post: myPost, author: user) }
 
   before { subject.save }
 
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,27 +3,32 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   user = User.create(name: 'Kibre', photo: 'link to photo', bio: 'Rails Developer')
 
-  subject {  Post.new(author: user, title: 'About Rais Rspec', text: 'Hello') }
+  subject { Post.new(author: user, title: 'About Rais Rspec', text: 'Hello') }
 
   before { subject.save }
 
   it 'Title must not be blank' do
     subject.title = nil
-    expect(subject).to_not be_valid 
+    expect(subject).to_not be_valid
   end
-  
+
   it 'Title must not exceed 250 characters' do
-    subject.title = 'a'*250
-    expect(subject).to_not be_valid 
+    subject.title = 'a' * 251
+    expect(subject).to_not be_valid
   end
-  
+
   it 'Comments counter must be greater than or equal to 0' do
     subject.comments_count = -1
-    expect(subject).to_not be_valid 
+    expect(subject).to_not be_valid
   end
- 
+
   it 'Likes counter must be greater than or equal to 0' do
     subject.likes_count = -1
-    expect(subject).to_not be_valid 
+    expect(subject).to_not be_valid
+  end
+
+  it 'is should show recent comments' do
+    comment = Comment.create(post: subject, author: user, text: 'my comment')
+    expect(comment.text).to eq('my comment')
   end
 end
