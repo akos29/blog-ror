4c6bcd19b6f870e634f293225c0fd2a40437b84f
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2023-03-03 21:38:23 +0200

    Fix Linter

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Like, type: :model do
   user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
-  myPost = Post.create(author: user, title: 'Hello', text: 'This is my first post')
+  my_post = Post.create(author: user, title: 'Hello', text: 'This is my first post')
 
-  subject { Like.new(post: myPost, author: user) }
+  subject { Like.new(post: my_post, author: user) }
 
   before { subject.save }
 
   it 'likes counter should be incremented' do
-    expect(myPost.likes_count).to eq 2
+    expect(my_post.likes_count).to eq 2
   end
 end
