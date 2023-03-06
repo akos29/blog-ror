137ebfbc9a4268fc3f7c9acfdf1b377004dc1a61
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2023-03-06 14:05:14 +0200

    Add controller for Post and User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,4 @@ class UsersController < ApplicationController
     # @users = User.find(params[:id])
     @recent_posts = Post.where(author_id: id)
   end
-
-  def edit
-    # @users = User.find(params[:id])
-  end
-
-  def show_recent_post
-    # @recent_posts = Post.where(author_id: self.id)
-  end
 end
