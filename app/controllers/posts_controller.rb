35cb9842bea4404d7632df09c36a2264bcfb77ac
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2023-03-06 21:48:57 +0200

    Update posts route

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,11 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.includes(:comments)
   end
 
   def show
-    @post = Post.find(params[:id])
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
   get 'home/about'
   
-  get 'posts', to: 'posts#index'
-  get 'posts/:id', to: 'posts#show'
-  get 'users', to: 'users#index'
-  get 'users/:id', to: 'users#show'
+ 
+  # get 'users', to: 'users#index'
+  # get 'users/:id', to: 'users#show', path: 'authors' do 
+  #   get 'posts', to: 'posts#index'
+  #   get 'posts/:id', to: 'posts#show'
+  # end
+  
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
+  
+
 
   root 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
