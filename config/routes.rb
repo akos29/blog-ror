63f4ec06c946bce8fedd3ea9a2ebbe54e4f55ddc
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2023-03-06 22:32:31 +0200

    Update routing for Users and update style

--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,16 @@ Rails.application.routes.draw do
   
  
   # get 'users', to: 'users#index'
-  # get 'users/:id', to: 'users#show', path: 'authors' do 
+  # get 'users/:id', to: 'users#show'
   #   get 'posts', to: 'posts#index'
   #   get 'posts/:id', to: 'posts#show'
   # end
-  
+  root 'users#index'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
+  # resource :users, only: [:index, :show]  
   
-
-
-  root 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
