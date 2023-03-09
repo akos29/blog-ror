532ec2f97cbde4fd605d39d69bff3d510a734ad8
Kibrewossen Y Mekasha <kibrish49@gmail.com>
2023-03-09 12:13:52 +0200

    Revert "Revert "Views""

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'comments/index'
+  get 'comments/show'
   get 'home/about'
   
  
@@ -9,7 +11,10 @@ Rails.application.routes.draw do
   # end
   root 'users#index'
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show] do
+      resources :comments, only: [:index, :show]
+      resources :likes, only: [:index, :show]
+    end
   end
   # resource :users, only: [:index, :show]  
   
