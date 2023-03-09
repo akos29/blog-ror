09e702b3234a6e642f30f36f4d6861e367f5373c
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2023-03-09 23:35:21 +0200

    Add route to new and create Comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,16 @@
 Rails.application.routes.draw do
+  get 'comments/new'
+  get 'comments/create'
   get 'comments/index'
   get 'comments/show'
   get 'home/about'
   
- 
-  # get 'users', to: 'users#index'
-  # get 'users/:id', to: 'users#show'
-  #   get 'posts', to: 'posts#index'
-  #   get 'posts/:id', to: 'posts#show'
-  # end
   root 'users#index'
   resources :users, only: [:index, :show] do
-    resources :posts do
+    resources :posts , only: [:index, :show, :new, :create] do
       resources :comments
-      resources :likes
+      resources :likes, only: [:new, :create]
     end
   end
-  # resource :users, only: [:index, :show]  
-  
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
