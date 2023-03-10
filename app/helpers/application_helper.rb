698a9dbb3fc3111b2dc776f81913ddf5d832ab07
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2023-03-10 15:30:38 +0200

    Add is_liked? method to manage like logic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def is_liked?(author_id, post_id)
+    post = Post.find_by(id: post_id) 
+    author = User.find_by(id: author_id)
+    liked = Like.where(author: author, post: post)
+  
+    liked.present?
+  end
 end
