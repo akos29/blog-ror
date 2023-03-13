d91c3b65418d0e0c056efbbb1f43309936858b3c
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2023-03-13 15:46:27 +0200

    Fix the N+1 issue on fetching comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,7 @@ class PostsController < ApplicationController
 
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts.includes(:comments).order(created_at: :desc)
+    @posts = @user.posts.order(created_at: :desc)
   end
 
   def new
