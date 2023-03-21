1dff73d8dd4f44c82e7a57d6441b09d2502974ed
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2023-03-21 23:59:26 +0200

    Update the test examples for Posts show.html.erb_spec.rb

--- spec/views/posts/show.html.erb_spec.rb
+++ spec/views/posts/show.html.erb_spec.rb
@@ -2,58 +2,47 @@ require 'rails_helper'
 RSpec.describe 'users#show', type: :feature do
   describe 'users show' do
     before(:each) do
-      @user1 = User.create!(name: 'Kibre', photo: 'https://pepe', bio: 'nobio', posts_counter: 0)
-      @user2 = User.create!(name: 'Pótamo', photo: 'https://pótamo', bio: 'some', posts_counter: 0)
-      @post1 = @user1.posts.create!(title: 'Pepe1 Post', text: 'Pepe1 text', comments_count: 0, likes_count: 0)
-      @post2 = @user1.posts.create!(title: 'Pepe2 Post', text: 'Pepe2 text', comments_count: 0, likes_count: 0)
-      @post3 = @user1.posts.create!(title: 'Pepe3 Post', text: 'Pepe3 text', comments_count: 0, likes_count: 0)
-      @post4 = @user1.posts.create!(title: 'Pepe4 Post', text: 'Pepe4 text', comments_count: 0, likes_count: 0)
-      @comment = @post1.comments.create!(text: 'Hi there!', author_id: @user2.id)
-      visit(user_path(@user1))
+      @user1 = User.create!(name: 'Abebe', photo: 'https://i.pravatar.cc/150?u=fake@pravatar.com',
+        bio: 'Teacher from Mexico.')
+@user2 = User.create!(name: 'Kebede', photo: 'https://i.pravatar.cc/150?u=fake@pravatar.com',
+                           bio: 'Teacher from Mexico.')
+      @post1 = @user1.posts.create!(title: 'Abebe1 Post', text: 'Abebe text')
+      @post2 = @user1.posts.create!(title: 'Abebe2 Post', text: 'Abebe2 text')
+      @post3 = @user1.posts.create!(title: 'Abebe3 Post', text: 'Abebe3 text')
+      @post4 = @user1.posts.create!(title: 'Abebe4 Post', text: 'Abebe4 text')
+      @comment1 = @post1.comments.create!(text: 'Hi there!', author_id: @user2.id)
+      @comment2 = @post1.comments.create!(text: 'Nice to hear from you again', author_id: @user1.id)
+      visit(user_posts_path(@user1))
     end
 
-    scenario "I can see the user's profile picture" do
-      expect(page).to have_xpath("//img[contains(@src,'https://pepe')]")
+    scenario "I can see the user's profile picture", positive: true do
+      expect(page).to have_xpath("//img[contains(@src,'https://i.pravatar.cc/150?u=fake@pravatar.com')]")
     end
 
-    scenario "I can see the user's username" do
-      expect(page).to have_content('Pepe')
+    scenario 'I can see who wrote the post', positive: true do
+      expect(page).to have_content(@user1.name)
     end
 
-    scenario 'I can see the number of posts the user has written' do
-      expect(page).to have_content('Number of posts: 4')
+    scenario 'I can see how many comments it has', positive: true do
+      expect(page).to have_content("Comments: #{@post1.comments_count}")
     end
 
-    scenario "I can see the user's bio" do
-      expect(page).to have_content('nobio')
+    scenario 'I can see how many likes it has' do
+      expect(page).to have_content("Likes: #{@post1.likes_count}")
     end
 
-    scenario "I can see the user's first 3 posts" do
-      expect(page).to have_content 'Pepe4 text'
-      expect(page).to have_content 'Pepe3 text'
-      expect(page).to have_content 'Pepe2 text'
+    scenario 'I can see the post body', positive: true do
+      expect(page).to have_content('Abebe text')
     end
 
-    scenario "I can see a button that lets me view all of a user's posts" do
-      expect(page).to have_link('See all posts')
+    scenario 'I can see the username of each commentor', positive: true do
+      expect(page).to have_content(@user1.name)
+      expect(page).to have_content(@user2.name)
     end
 
-    scenario "When I click to see all posts, it redirects me to the user's posts index page" do
-      click_link 'See all posts'
-      expect(page).to have_current_path("/users/#{@user1.id}/posts/")
-    end
-  end
-
-  describe 'click detail' do
-    before(:each) do
-      @user = User.create!(name: 'Pepe', photo: 'https://pepe', bio: 'nobio', posts_counter: 0)
-      @post = @user.posts.create!(title: 'Pepe1 Post', text: 'Pepe1 text', comments_count: 0, likes_count: 0)
-      visit(user_path(@user))
-    end
-
-    scenario "When I click a user's post, it redirects me to that post's show page" do
-      click_link 'Detail'
-      expect(page).to have_current_path user_post_path(@user, @post)
+    scenario 'I can see the comment each commentor left', positive: true do
+      expect(page).to have_content('Hi there!')
+      expect(page).to have_content('Nice to hear from you again')
     end
   end
 end
