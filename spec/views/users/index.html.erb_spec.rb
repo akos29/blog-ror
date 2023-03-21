21a06d41c0491608c020cf1d00de6b4aaec811a3
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2023-03-21 21:45:48 +0200

    Add test examples for users index.html.erb

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -3,31 +3,27 @@ require 'rails_helper'
 RSpec.describe 'users#index', type: :feature do
   describe 'users index page' do
     before(:each) do
-      User.create!(name: 'Abebe', photo: 'https://i.pravatar.cc/150?u=fake@pravatar.com',
+      @user1 = User.create!(name: 'Abebe', photo: 'https://i.pravatar.cc/150?u=fake@pravatar.com',
                    bio: 'Teacher from Mexico.')
-      User.create!(name: 'Kebede', photo: 'https://i.pravatar.cc/150?u=fake@pravatar.com',
+      @user2 = User.create!(name: 'Kebede', photo: 'https://i.pravatar.cc/150?u=fake@pravatar.com',
                    bio: 'Teacher from Mexico.')
       visit root_path
-      @user = User.first!
     end
 
     scenario 'I can see the username of all other users.' do
       expect(page).to have_content('Abebe')
-      expect(page).to have_content('Kebede')
     end
 
     it 'I can see the profile picture for each user.' do
-      expect(page).to have_xpath('')
-      expect(page).to have_xpath("//img[contains(@src,'https://avatars.steamstatic.com/75bbbe287e759d46c539b54cc7c70e4acd14087c_full.jpg')]")
+      expect(page).to have_xpath("//img[contains(@src,'https://i.pravatar.cc/150?u=fake@pravatar.com')]")
     end
-
     it 'I can see the number of posts each user has written.' do
-      expect(page).to have_content('Number of posts: 2')
+      expect(page).to have_content('Number of Posts: 0')
     end
 
     it "When I click on a user, I am redirected to that user's show page" do
-      visit(users_path(@user.id))
-      expect(page).to have_content('abebe')
+      visit(users_path(@user1.id))
+      expect(page).to have_content('Abebe')
       expect(page).not_to have_content('Pótamo')
     end
   end
