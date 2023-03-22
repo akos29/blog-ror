689a0dd26b519537f956a41c3e2f70ceeb169b8e
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2023-03-22 16:55:57 +0200

    Add auto generated sign-up and validation

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,7 @@ Rails.application.routes.draw do
     end
   end
 
+  get 'users/:user_id/posts_list', to: 'api#list_all_user_posts', as: :list_all_user_posts
+  get 'users/:user_id/posts/:post_id/comments_list', to: 'api#list_all_post_comments', as: :list_all_post_comments
+  post 'users/:user_id/posts/:post_id/add_comment', to: 'api#add_comment', as: :add_comment
 end
