153d8fb1711d16babaef9b9630f43ffd0bb702e9
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2023-03-22 16:17:45 +0200

    add generate devise user

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,10 @@
 #  posts_count :integer
 #
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   attribute :posts_count, :integer, default: 0
   validates :name, presence: true, length: { minimum: 2 }
   validates :posts_count, comparison: { greater_than_or_equal_to: 0 }
