f72c617475a93878781fe56a82e6e04aa6389773
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2023-03-22 17:22:23 +0200

    Add logout feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-    before_action :update_allowed_parameters, if: :devise_controller?
-  
-   def current_user
-      User.find(params[:user_id])
-  end 
-  
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  def sign_out
+    current_user.sign_out
+  end
+
   protected
-  
-    def update_allowed_parameters
-      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :surname, :email, :password)}
-      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :surname, :email, :password, :current_password)}
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :surname, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :surname, :email, :password, :current_password)
     end
-    
-  
+  end
+
+
 
   helper_method :current_user
 end
