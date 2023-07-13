04e63f8839d3914285e6ef697852bbb2f8ad2554
Kibrewossen Y Mekasha <kibrish49@gmail.com>
2023-07-13 15:20:42 +0200

    Update comments model as per the comment provided

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,15 +1,6 @@
 # == Schema Information
 #
-# Table name: comments
-#
-#  id         :bigint           not null, primary key
-#  text       :text
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  author_id  :bigint           not null
-#  post_id    :bigint           not null
-#
 class Comment < ApplicationRecord
-  belongs_to :users
-  belongs_to :posts
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
 end
