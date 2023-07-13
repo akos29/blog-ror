45ce98437ede3e03c996e52156ad1745bbb03208
Kibrewossen Y Mekasha <kibrish49@gmail.com>
2023-07-13 17:51:46 +0200

    Update recent comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,10 @@ class Post < ApplicationRecord
   has_many :comment
   has_many :like
   
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
   private
 
   def update_posts_counter
