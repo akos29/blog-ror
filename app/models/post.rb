5d2f2a894ac29d10f53f93ffeb1fb1f6b5a73ffd
Kibrewossen Y Mekasha <kibrish49@gmail.com>
2023-07-13 15:30:09 +0200

    Update update_post_counter method

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,13 @@
 class Post < ApplicationRecord
+  after_save :update_post_counter
+
+  private
+
+  def update_post_counter
+    author.increment!(:posts_counter)
+  end
+    
   belongs_to :author, class_name: 'User'
-  has_many :comments
-  has_many :likes
+  has_many :comment
+  has_many :like
 end
