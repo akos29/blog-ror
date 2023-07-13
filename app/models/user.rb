165efb382a24bf0c7dd22d96e095e17e3f85564a
Kibrewossen Y Mekasha <kibrish49@gmail.com>
2023-07-13 15:13:48 +0200

    Update foreign_key as per the comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,5 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id         :bigint           not null, primary key
-#  name       :string
-#  bio        :text
-#  photo      :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
 class User < ApplicationRecord
-  has_many :posts
-  has_many :comments
-  has_many :likes
+  has_many :posts, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
+  has_many :likes, foreign_key, 'author_id'
 end
