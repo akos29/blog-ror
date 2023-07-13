87ca33db277f12c4bbdb689f16659e5277762eaa
Kibrewossen Y Mekasha <kibrish49@gmail.com>
2023-07-13 15:16:34 +0200

    Update reference to Post and User as per the comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,5 @@
-# == Schema Information
-#
-# Table name: posts
-#
-#  id         :bigint           not null, primary key
-#  title      :string
-#  text       :text
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  author_id  :bigint           not null
-#
 class Post < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
 end
