2d2255661cbf6638005e4190dfcd5dfcceb2554e
Kibrewossen Y Mekasha <kibrish49@gmail.com>
2023-07-20 00:15:46 +0200

    Fix the  issues with the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 user = User.create!(
   name: 'Abebe',
   bio: 'Story of Abe\'s life ...',
-  photo: 'url'
+  photo: 'https://i.pravatar.cc/300'
 )
-second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
-first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+second_user = User.create(name: 'Lilly', photo: 'https://i.pravatar.cc/300', bio: 'Teacher from Poland.')
+first_user = User.create(name: 'Tom', photo: 'https://i.pravatar.cc/300', bio: 'Teacher from Mexico.')
 
 
 first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
