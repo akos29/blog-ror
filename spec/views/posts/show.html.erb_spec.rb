e78afe2b42c55e4832b025e5c521409a5b668d61
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-07-26 10:51:01 +0300

    Add some negative tests to posts#show view

--- spec/views/posts/show.html.erb_spec.rb
+++ spec/views/posts/show.html.erb_spec.rb
@@ -27,12 +27,24 @@ RSpec.describe 'users#show', type: :feature do
       expect(page).to have_content("Comments: #{@post1.comments_count}")
     end
 
+    scenario 'I should see the correct number of comments', negative: true do
+      expect(page).to_not have_content("Comments: #{@post1.comments_count + 1}")
+    end
+
     scenario 'I can see how many likes it has' do
       expect(page).to have_content("Likes: #{@post1.likes_count}")
     end
 
+    scenario 'I should see the correct number of likes' do
+      expect(page).to_not have_content("Likes: #{@post1.likes_count + 1}")
+    end
+
     scenario 'I can see the post body', positive: true do
       expect(page).to have_content('Abebe text')
+    end
+
+    scenario 'I can see the correct post body', negative: true do
+      expect(page).to_not have_content('Abebe text' + 'some other text')
     end
 
     scenario 'I can see the username of each commentor', positive: true do
@@ -43,6 +55,10 @@ RSpec.describe 'users#show', type: :feature do
     scenario 'I can see the comment each commentor left', positive: true do
       expect(page).to have_content('Hi there!')
       expect(page).to have_content('Nice to hear from you again')
+    end
+
+    scenario 'I should not see any other comments text', negative: true do
+      expect(page).to_not have_content('some other comments')
     end
   end
 end
